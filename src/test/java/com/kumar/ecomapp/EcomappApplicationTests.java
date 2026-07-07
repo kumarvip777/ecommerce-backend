7808af6d44c5604d96d151d5de62723b641945e8
kumarvip777 <85467556+kumarvip777@users.noreply.github.com>
2026-07-07 13:18:31 +0530

    "initialize Spring Boot project"

---
+++ src/test/java/com/kumar/ecomapp/EcomappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumar.ecomapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
